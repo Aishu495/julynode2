c79e87afdab0644d941b697a24ba74d310dd8fe0
Aishu495 <aishwaryad65@gmail.com>
2021-12-02 18:15:07 +0530

    a

--- app.js
+++ app.js
@@ -180,6 +180,23 @@ app.put('/updateStatus/:id',(req,res) => {
     )
 })
 
+app.put('/updateOrder/:id',(req,res) => {
+    var id = Number(req.params.id);
+    var status = req.body.status?req.body.status:"Pending"
+    db.collection('orders').updateOne(
+        {id:id},
+        {
+            $set:{
+                "date":req.body.date,
+                "bank_status":req.body.bank_status,
+                "bank":req.body.bank,
+                "status":status
+            }
+        }
+    )
+    res.send('data updated')
+})
+
 MongoClient.connect(mongourl, (err,client) => {
     if(err) console.log("Error While Connecting");
     db = client.db('zomato');
